3bdf40c0a130b0dcc8c659f83fb0ff4dadc80333
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-19 16:31:08 -0300

    Implementa funções validadoras para os campos do formulário

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,46 @@ import React from 'react';
 import { Redirect } from 'react-router';
 
 class Login extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      isEmailValid: false,
+      isPasswordValid: false,
+      isButtonDisabled: true,
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    if(target.name === 'password') {
+      if(target.value.length >= 5) {
+        this.setState({
+          isPasswordValid: true,
+        });
+      }
+    } else {
+      const hasAt = target.value.split('').find((char) => '@');
+      const hasDotCom = target.value.endsWith('.com');
+      if(hasAt && hasDotCom) {
+        this.setState({
+          isEmailValid: true,
+        });
+      }
+    }
+    const { isEmailValid, isPasswordValid } = this.state;
+    if(isEmailValid && isPasswordValid ) {
+      this.setState({
+        isButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isButtonDisabled: true,
+      });
+    }
+  }
   render() {
     const { isButtonDisabled } = this.state;
     return (
