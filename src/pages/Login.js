08436039fab952e57597046959effe4601e9e445
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-19 16:30:34 -0300

    Cria formulário da página de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 
 class Login extends React.Component {
+
   render() {
-    return <div>Login</div>;
+    const { isButtonDisabled } = this.state;
+    return (
+      <main>
+        <form>
+          <input data-testid="email-input" type="email" name="email" placeholder="Enter your email..." onChange={ this.handleChange } />
+          <input data-testid="password-input" type="password" name="password" placeholder="Enter your password..." onChange={ this.handleChange } />
+
+          <button type="button" disabled={ isButtonDisabled }>Entrar</button>
+        </form>
+      </main>
+    );
   }
 }
 
