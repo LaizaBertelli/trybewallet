f339b4d0cb14d534b1364f44b273db61bfb77ed8
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-19 16:28:43 -0300

    Cria reducer inicial padrão

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,17 @@
-// import user from './user';
-// import wallet from './wallet';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const INITIAL_STATE = [
+  {
+    user: '',
+    wallet: '',
+  }
+];
+
+function listReducer(state = INITIAL_STATE, action) {
+  return state;
+}
+
+export default listReducer;
