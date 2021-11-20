8d3d5f1c82c8d4314b3271c8de118fa908bc4a0d
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-20 19:14:34 -0300

    Cria e renderiza o componente Header

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from '../components/Header.js';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <Header />
+    );
   }
 }
 
