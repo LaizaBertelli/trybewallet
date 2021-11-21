526887c6ad6dd4323e21e18eef9019fd872d482d
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-20 21:13:36 -0300

    Implementa reducer para informações na wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = [
+  {
+    expense: '',
+    description: '',
+    currency: '',
+    method: '',
+    tag: '',
+  },
+];
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'SAVE_WALLET':
+    return action.value;
+  default:
+    return state;
+  }
+}
+
+export default wallet;
