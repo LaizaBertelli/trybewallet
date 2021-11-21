8331ad6a479920ac9b4c120a8a72d05aeb9d7b65
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-21 18:02:06 -0300

    Renderiza informações da wallet na tabela

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,25 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Expenses extends React.Component {
+  constructor() {
+    super();
+
+    this.getTableData = this.getTableData.bind(this);
+    this.getCurrencyName = this.getCurrencyName.bind(this);
+  }
+
+  // Função para recuperar o nome por extenso da moeda
+  getCurrencyName(exchanges, code) {
+    const { name } = exchanges[code];
+    return name.split('/')[0];
+  }
+
+  getTableData(expenses) {
+    const { description, method, tag, exchangeRates, currency, value, id } = expenses;
+    const currencyName = this.getCurrencyName(exchangeRates, currency);
+    const { ask } = exchangeRates[currency];
+    const exchangeFixed = parseFloat(ask).toFixed(2);
+    return (
+      <tbody key={ id }>
+        <tr name={ id }>
+          <td>{ description }</td>
+          <td>{ tag }</td>
+          <td>{ method }</td>
+          <td>{ value }</td>
+          <td>{ currencyName }</td>
+          <td>{ exchangeFixed }</td>
+          <td>{ convertValue(value, ask) }</td>
+          <td>Real</td>
+        </tr>
+      </tbody>
+    );
+  }
+
+  convertValue(exchangeValue, currentAsk) {
+    return (exchangeValue * currentAsk).toFixed(2);
+  }
+
   render() {
+    const { wallet } = this.props;
     return (
       <div className="expenses-table">
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          {
+            wallet.expenses !== undefined && (
+              wallet.expenses.map((expense) => this.getTableData(expense))
+            )
+          }
         </table>
       </div>
     );
   }
 }
 
-export default Expenses;
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+Expenses.propTypes = {
+  wallet: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Expenses);
