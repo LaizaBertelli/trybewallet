744f7964d178d433678ae66df636eb1116035e72
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-21 15:25:40 -0300

    Corrige erro na implementação do recuer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,14 +1,19 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const INITIAL_STATE = [
   {
-    expenses: [],
+    id: '',
+    value: 0,
+    description: '',
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+    exchangeRates: {},
   },
 ];
-
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'SAVE_WALLET':
-    return action.value;
+    return { expenses: action.value };
   default:
     return state;
   }
