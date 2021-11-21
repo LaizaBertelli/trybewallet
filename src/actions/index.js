88ff4c963a0bc5533bb273edbdbe049ad5d71796
LaizaBertelli <laizabertelli@hotmail.com>
2021-11-21 15:26:39 -0300

    Implementa action para consultar API

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,22 @@ export const saveWallet = (value) => ({
   type: 'SAVE_WALLET',
   value,
 });
+
+// Funções criadas com base nos exemplos do Course
+const requestCurrencies = () => ({
+  type: 'REQUEST_CURRENCY',
+});
+
+const receiveCurrencies = (currencies) => ({
+  type: 'RECEIVE_CURRENCY',
+  currencies,
+});
+
+export function fetchCurrency() {
+  return (dispatch) => {
+    dispatch(requestCurrencies());
+    return fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((currencies) => dispatch(receiveCurrencies(currencies)));
+  };
+}
